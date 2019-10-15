226ffd095fac721e33e91ab5627a48cecafd761e
Bryce Boe <bbzbryce@gmail.com>
2019-10-15 16:15:04 -0700

    Only show calculators once button is pressed

--- class_app/src/App.jsx
+++ class_app/src/App.jsx
@@ -5,8 +5,12 @@ import Calculator from "./Calculator";
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { counter: 0 };
+    this.state = { counter: 0, openCalculator: false };
   }
+
+  startCalculators = () => {
+    this.setState({ openCalculator: true });
+  };
 
   updateCounter = () => {
     console.log("Update");
@@ -16,11 +20,22 @@ class App extends React.Component {
   };
 
   render() {
+    let node;
+    if (this.state.openCalculator) {
+      node = (
+        <>
+          <Calculator updateCounter={this.updateCounter} />
+          <Calculator updateCounter={this.updateCounter} />
+        </>
+      );
+    } else {
+      node = <button onClick={this.startCalculators}>Start calculators</button>;
+    }
+
     return (
       <>
         <div>Number of calculations: {this.state.counter}</div>
-        <Calculator updateCounter={this.updateCounter} />
-        <Calculator updateCounter={this.updateCounter} />
+        {node}
       </>
     );
   }
