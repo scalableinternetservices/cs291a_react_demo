82f796ec0c8389032bb7891b5f8099126470fdfb
Bryce Boe <bbzbryce@gmail.com>
2019-10-15 15:42:50 -0700

    Convert function to class and add onChange handler

--- class_app/src/App.jsx
+++ class_app/src/App.jsx
@@ -2,14 +2,40 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  return (
-    <form>
-      <input name="lhs" type="text" />
-      <input name="rhs" type="text" />
-      <input type="submit" value="Calculate" />
-    </form>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { lhs: "", rhs: "" };
+  }
+
+  onChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  render() {
+    return (
+      <>
+        <form>
+          <input
+            name="lhs"
+            type="text"
+            onChange={this.onChange}
+            value={this.state.lhs}
+          />
+          <br />
+          <input
+            name="rhs"
+            type="text"
+            onChange={this.onChange}
+            value={this.state.rhs}
+          />
+          <br />
+          <input type="submit" value="Calculate" />
+        </form>
+        <div>Enter two numbers and press "Calculate" to get the sum.</div>
+      </>
+    );
+  }
 }
 
 export default App;
