378136109274cdd307aea9038743e7ef934c6d53
Bryce Boe <bbzbryce@gmail.com>
2021-01-21 15:54:18 -0800

    Add initial calculator view

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <input name="left" />
+      <input name="right" />
+      <input type="submit" value="Calculate!" />
+    </div>
+  );
+}
+
+export default App;
