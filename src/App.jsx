c6f5c8a50fb87371f4326dbcdfc995ddc6bbc324
Bryce Boe <bbzbryce@gmail.com>
2021-01-21 16:03:47 -0800

    Update left and right with updateInput callback

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      left: "",
+      right: ""
+    }
+  }
+
+  updateInput = (event) => {
+    this.setState({[event.target.name]: event.target.value});
+  }
+
+  render() {
+    return (
     <div className="App">
-      <input name="left" />
-      <input name="right" />
+      <input name="left" onChange={this.updateInput} value={this.state.left} />
+      <input name="right" onChange={this.updateInput} value={this.state.right} />
       <input type="submit" value="Calculate!" />
     </div>
   );
+  }
 }
 
 export default App;
